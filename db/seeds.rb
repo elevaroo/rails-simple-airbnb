bd5fbfab07ec78152dd96179620314b81fb64abc
Eleonore van Roosendaal <eleonore.roosendaal@hotmail.com>
2020-11-18 16:50:23 +0100

    Created the index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+puts 'database is clean'
+
+8.times do
+  p Flat.create!(
+    name: Faker::GreekPhilosophers.name,
+    address: Faker::Address.street_address,
+    description: Faker::GreekPhilosophers.quote,
+    price_per_night: rand(50..200),
+    number_of_guests: rand(2..8)
+  )
+end
